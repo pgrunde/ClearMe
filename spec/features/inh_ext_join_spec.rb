d3994a378842ffdff9a35910dfde36ecd1327d68
Peter Grunde <peter.a.grunde@gmail.com>
2014-10-02 23:14:16 -0600

    wrote some contract tests

--- spec/features/inh_ext_join_spec.rb
+++ spec/features/inh_ext_join_spec.rb
@@ -2,17 +2,46 @@ require 'rails_helper'
 require 'capybara/rails'
 
 
-describe "in house users can search for existing recruitment firms" do
+describe "in order for both users to forge a contract together", focus: true do
 
-  it "in house user can view a list of external recruiters" do
-    inhouse_user = InhouseUser.create(username: "kipperton",
+  before :each do
+    @inhouse_user = InhouseUser.create(username: "kipperton",
                                       email: "kipper@example.com",
                                       password_digest: "password")
-    external_user = ExternalUser.create(username: "nora",
-                                       email:"nora@example.com",
-                                       password_digest: "password")
-    page.set_rack_session(:inhouse_user_id => inhouse_user.id)
-    visit
+    @external_user = ExternalUser.create(username: "nora",
+                                        email:"nora@example.com",
+                                        password_digest: "password")
+    page.set_rack_session(:inhouse_user_id => @inhouse_user.id)
+    visit search_for_ext_path
   end
 
+  it "in house user can view a list of external recruiters with unpending contracts and request them" do
+    expect(page).to have_content "nora"
+    click_link "Request Contract"
+    expect(page).to_not have_content "nora"
+  end
+
+  it "external user can view messages to notify a sent contract" do
+    click_link "Request Contract"
+    page.set_rack_session(:external_user_id => @external_user.id)
+    visit ext_mainpage_path
+    click_link "Pending Contract Requests"
+    expect(page).to have_content "kipperton"
+  end
+
+  it "external user can approve a contract request and an inhouse user can be notified" do
+    click_link "Request Contract"
+    page.set_rack_session(:external_user_id => @external_user.id)
+    visit ext_mainpage_path
+    click_link "Pending Contract Requests"
+    click_link "Send Contract"
+    expect(page).to_not have_content "kipperton"
+    page.set_rack_session(:inhouse_user_id => @inhouse_user.id)
+  end
+
+  
+
 end
+    # visit external_contracts_path
+    # click_link "Approve Pending Contracts"
+    # click_link "Approve Contract!"
